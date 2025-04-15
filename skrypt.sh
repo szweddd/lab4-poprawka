9e500fb594f9476fb1fbcc6fabf4f025d90f428e
szweddd <szwed.tak.o@gmail.com>
2025-04-15 19:25:46 +0200

    Dodano --init do klonowania repo i ustawienia PATH

---
+++ skrypt.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if [[ "$1" == "--date" ]]; then
+  date
+fi
+
+if [[ "$1" == "--logs" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    filename="log$i.txt"
+    echo -e "$filename\nskrypt.sh\n$(date)" > "$filename"
+  done
+fi
+
+if [[ "$1" == "--help" ]]; then
+  echo "--date        pokazuje dzisiejszą datę"
+  echo "--logs [N]    tworzy N plików logX.txt"
+  echo "--help        pokazuje dostępne opcje"
+fi
+
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+  date
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    filename="log$i.txt"
+    echo -e "$filename\nskrypt.sh\n$(date)" > "$filename"
+  done
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "--date | -d        pokazuje dzisiejszą datę"
+  echo "--logs [N] | -l N  tworzy N plików logX.txt"
+  echo "--help | -h        pokazuje dostępne opcje"
+fi
+
+elif [[ "$1" == "--init" ]]; then
+  git clone https://github.com/szweddd/lab4-poprawka.git cloned_repo
+  echo "export PATH=\$PATH:$(pwd)/cloned_repo" >> ~/.bashrc
+  source ~/.bashrc
+fi
